220c82a1399891d7bd6c1d78761f561ca6b52857
cubicroot <github@cubicroot.xyz>
2023-10-27 23:09:36 +0200

    preload assets

---
+++ preload_assets.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# Preloaded sprites.
+var tree0 = preload("res://assets/Trees and Bushes/Tree_Dark_2.png")
+var tree1 = preload("res://assets/Trees and Bushes/Tree_Light_2.png")
+var tree2 = preload("res://assets/Trees and Bushes/Tree_Red_2.png")
+var tree3 = preload("res://assets/Trees and Bushes/Tree_Snow_2.png")
+
+var rock0 = preload("res://assets/Rocks/Rock_Brown_3.png")
+var rock1 = rock0
+var rock2 = rock0
+var rock3 = preload("res://assets/Rocks/Rock_Brown_Snow_3.png")
+
+var grain0 = preload("res://assets/Trees and Bushes/Bush_Dark_2.png")
+var grain1 = preload("res://assets/Trees and Bushes/Bush_Light_2.png")
+var grain2 = preload("res://assets/Trees and Bushes/Bush_Red_2.png")
+var grain3 = preload("res://assets/Trees and Bushes/LeavyBush_Red_4.png")
