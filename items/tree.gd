18cb1e482bb361c89785f5656a8986c3e4648f07
Marvin Altemeier <marvin.altemeier@hetzner-cloud.de>
2023-10-28 01:32:29 +0200

    Move texture logic from harvestable to tree

--- items/tree.gd
+++ items/tree.gd
@@ -1,14 +1,33 @@
 extends StaticBody2D
 
-# Settings!
-# Spawnrate is the probability that an item is harvested.
-var spawnrate = 0.5
-# Seasons the item is active.
-var seasonsActive = [Game.Season.SPRING, Game.Season.FALL, Game.Season.WINTER]
+@onready var timer = $Timer
+
 # Season textures.
+@export var season = Game.Season.SPRING
 var textures = {
 	0: Game.tree0,
 	1: Game.tree1,
 	2: Game.tree2,
 	3: Game.tree3
 }
+
+@onready var texture = get_node("texture")
+
+func _process(delta):
+	_update_season(Game.season)
+
+# _update_season sets the items season.
+func _update_season(newSeason):
+	if season == newSeason:
+		return
+
+	# Update item texture.
+	timer.wait_time = randf_range(0, 5)
+	timer.start()
+
+
+	season = newSeason
+
+
+func _on_timer_timeout():
+	texture.set_texture(textures[Game.season])
