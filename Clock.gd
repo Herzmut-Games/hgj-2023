6668ecbc7430862883ebde9396e9f65d85a08643
Marvin Altemeier <marvin.altemeier@hetzner-cloud.de>
2023-10-27 22:55:01 +0200

    Change season from clock scene

--- Clock.gd
+++ Clock.gd
@@ -15,3 +15,19 @@ func _process(delta):
 
 	seasons.set_rotation(seasons.get_rotation() - ROTATION_SPEED * delta)
 	pass
+
+func _season_progress(delta):
+	seasons.set_rotation(seasons.get_rotation() - ROTATION_SPEED * delta)
+
+	var rot = seasons.get_rotation()
+
+	if rot >= 0 and rot < 90:
+		Game.set_season(Game.Season.SPRING)
+	elif rot >= 90 and rot < 180:
+		Game.set_season(Game.Season.SUMMER)
+	elif rot >= 180 and rot < 270:
+		Game.set_season(Game.Season.FALL)
+	elif rot >= 270 and rot < 360:
+		Game.set_season(Game.Season.WINTER)
+
+	pass
