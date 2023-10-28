0574fb0e3b64bbe3ea8865e328e52790bfb983f8
Marvin Altemeier <marvin.altemeier@hetzner-cloud.de>
2023-10-28 09:38:14 +0200

    Add hunger bar

--- game.gd
+++ game.gd
@@ -2,8 +2,10 @@ extends Node
 
 enum Season { SPRING, SUMMER, FALL, WINTER }
 
-# season 0 = spring, 1 = summer, 2 = fall, 3 = winter.
-@export var season = Season.SPRING
+var season = Season.SPRING
+var hunger_level = 10
+
+var hunger_rate = 0.1
 
 # Preloaded sprites.
 var tree0 = preload("res://assets/Trees and Bushes/Tree_Dark_2.png")
@@ -18,7 +20,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	_tick_hunger(delta)
 	pass
+
+func _tick_hunger(delta):
+	hunger_level -= hunger_rate * delta
+	if hunger_level < 0:
+		hunger_level = 0
 
 func set_season(s):
 	season = s
---
+++ hunger_bar.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var progress_bar
+
+@export var hunger_level = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	progress_bar = get_node("ProgressBar")
+	hunger_level = Game.hunger_level
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	_check_hunger()
+	print(hunger_level)
+	pass
+
+func _check_hunger():
+	hunger_level = Game.hunger_level
+	progress_bar.value = hunger_level
