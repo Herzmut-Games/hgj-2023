ff3622ebac3d90bfc02283d1ca4602855e4ca2e8
Marvin Altemeier <marvin.altemeier@hetzner-cloud.de>
2023-10-28 15:53:09 +0200

    Add required resources to campfire

--- Campfire.gd
+++ Campfire.gd
@@ -1,5 +1,12 @@
 extends StaticBody2D
 
+@onready var required_resources = $RequiredResources
+
+func _ready():
+	required_resources.set_required({
+		Game.Items.FOOD: 1
+	})
+
 func interact(area):
 	if area.is_in_group("player"):
 		if Game.has_item(Game.Items.FOOD, 1):
